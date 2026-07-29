8550c9c6ee5255b76760f09e90941cb3d591a433
Cosmo <neto-lp@live.com>
2026-07-29 12:26:33 -0300

    personal info resumeform done

--- frontend/src/components/ResumeForm.tsx
+++ frontend/src/components/ResumeForm.tsx
@@ -1,4 +1,5 @@
-import { Resume } from "../types/resume";
+import type React from "react";
+import type { Resume } from "../types/resume";
 
 function ResumeForm({
   resume,
@@ -16,14 +17,110 @@ function ResumeForm({
       },
     });
   }
+  function handleEmailChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setResume({
+      ...resume,
+      personal: {
+        ...resume.personal,
+        email: e.target.value,
+      },
+    });
+  }
+  function handlePhoneChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setResume({
+      ...resume,
+      personal: {
+        ...resume.personal,
+        phone: parseInt(e.target.value),
+      },
+    });
+  }
+
+  function handleGithubChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setResume({
+      ...resume,
+      personal: {
+        ...resume.personal,
+        github: e.target.value,
+      },
+    });
+  }
+
+  function handleLinkedinChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setResume({
+      ...resume,
+      personal: {
+        ...resume.personal,
+        linkedin: e.target.value,
+      },
+    });
+  }
+
+  function handleWebsiteChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setResume({
+      ...resume,
+      personal: {
+        ...resume.personal,
+        website: e.target.value,
+      },
+    });
+  }
 
   return (
-    <input
-      type="text"
-      placeholder="Your name"
-      value={resume.personal.fullName}
-      onChange={handleNameChange}
-    />
+    <>
+      <label>
+        Nome:
+        <input
+          required
+          type="text"
+          placeholder="Seu nome"
+          value={resume.personal.fullName}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          required
+          type="email"
+          placeholder="Email"
+          value={resume.personal.email}
+          onChange={handleEmailChange}
+        />
+      </label>
+      <label>
+        Telefone:
+        <input required type="tel" placeholder="(00) 0000-0000" 
+        onChange={handlePhoneChange}
+        />
+      </label>
+      <label>
+        Github:
+        <input
+          type="text"
+          placeholder="https://github.com/username"
+          value={resume.personal.github}
+          onChange={handleGithubChange}
+
+        />
+      </label>
+      <label htmlFor="">
+        {" "}
+        Linkedin
+        <input value={resume.personal.linkedin} 
+        onChange={handleLinkedinChange}
+        />
+      </label>
+      <label>
+        Website:
+        <input
+          type="text"
+          placeholder="https://www.website.com.br"
+          value={resume.personal.website}
+          onChange={handleWebsiteChange}
+        />
+      </label>
+    </>
   );
 }
 
